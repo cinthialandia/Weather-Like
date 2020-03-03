8a850887c4c36a381aed80496616b864e640e09a
Cinthia Valero <cinthia.j.23@gmail.com>
2020-03-03 23:30:28 +1100

    lat and lon api ready

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
-const API_URL = "http://api.openweathermap.org/data/2.5/weather";
+const WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather";
+const ULTRAVIOLET_API_URL = "http://api.openweathermap.org/data/2.5/uvi?appid=";
 const API_ID = "849b886201ff69a029a2a86bc89f394a";
 
 export async function getCityWeather(city = "sydney", state = "nsw") {
-  const requestURL = `${API_URL}?q=${city},${state}&appid=${API_ID}`;
+  const requestURL = `${WEATHER_API_URL}?q=${city},${state}&appid=${API_ID}`;
   const response = await fetch(requestURL);
   const weather = await response.json();
 
@@ -43,3 +44,16 @@ function convertKelvinToFahrenheit(temperature) {
   //Rounding down fahrenheit
   return Math.floor(kelvinTofahrenheit);
 }
+
+async function getUltravioletIndex(lat, lon) {
+  async function getLatAndLon() {
+    const weather = await getCityWeather();
+    const lat = weather.coord.lat;
+    const lon = weather.coord.lon;
+  }
+  getLatAndLon();
+}
+
+getUltravioletIndex();
+
+//const requestURL = `${ULTRAVIOLET_API_URL}${API_ID}&lat=${lat}&lon=${lon}`;
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import { getCityWeather } from "./api.js";
 
 const getCityNameElm = document.querySelector(".cityName");
-const getWeatherImgElm = document.querySelector(".weatherImg");
-console.log(getCityNameElm);
+const getWeatherImgElm = document.querySelector(".wi");
+const getTemperatureElm = document.querySelector(".temperature");
+const getMaxAndMinTemperatureElm = document.querySelector(
+  ".maxAndMinTemperature"
+);
+const getUvVIndexElm = document.querySelector(".UvVbIndex");
+console.log(getUvVIndexElm);
 
 async function init() {
   const weather = await getCityWeather();
-
-  // render
+  //Inyectando el nombre de la ciudad
+  getCityNameElm.innerHTML = weather.name;
+  //inyectando la temperatura
+  getTemperatureElm.innerHTML = `${weather.main_c.temp}°`;
+  //inyectando la temperatura min y max
+  getMaxAndMinTemperatureElm.innerHTML = `${weather.main_c.temp_min}° / ${weather.main_c.temp_max}°`;
 
   console.log(weather);
 }
