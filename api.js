a3bbdf985fb7601d3d8d43d33b2aa35e3180327f
Cinthia Valero <cinthia.j.23@gmail.com>
2020-03-03 21:57:18 +1100

    get city weather api

---
+++ api.js
@@ -0,0 +1,45 @@
+const API_URL = "http://api.openweathermap.org/data/2.5/weather";
+const API_ID = "849b886201ff69a029a2a86bc89f394a";
+
+export async function getCityWeather(city = "sydney", state = "nsw") {
+  const requestURL = `${API_URL}?q=${city},${state}&appid=${API_ID}`;
+  const response = await fetch(requestURL);
+  const weather = await response.json();
+
+  // pasar temperaturas a C
+  weather.main_c = {
+    ...weather.main,
+    temp: convertKelvinToCelsius(weather.main.temp),
+    feels_like: convertKelvinToCelsius(weather.main.temp),
+    temp_min: convertKelvinToCelsius(weather.main.temp_min),
+    temp_max: convertKelvinToCelsius(weather.main.temp_max)
+  };
+
+  // pasar temperaturas a F
+  weather.main_f = {
+    ...weather.main,
+    temp: convertKelvinToFahrenheit(weather.main.temp),
+    feels_like: convertKelvinToFahrenheit(weather.main.temp),
+    temp_min: convertKelvinToFahrenheit(weather.main.temp_min),
+    temp_max: convertKelvinToFahrenheit(weather.main.temp_max)
+  };
+
+  return weather;
+}
+
+function convertKelvinToCelsius(temperature) {
+  // Save in a variable kelvin temperature
+  const kelvin = temperature;
+  //converting kelvin to Celsius
+  const kelvinToCelsius = kelvin - 273;
+  return Math.floor(kelvinToCelsius);
+}
+
+function convertKelvinToFahrenheit(temperature) {
+  // Save in a variable kelvin temperature
+  const kelvin = temperature;
+  //Convert kelvin to Fahrenheit
+  const kelvinTofahrenheit = (kelvin - 273.15) * 1.8 + 32;
+  //Rounding down fahrenheit
+  return Math.floor(kelvinTofahrenheit);
+}
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-async function weatherApi() {
-  const response = await fetch(
-    "http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=849b886201ff69a029a2a86bc89f394a"
-  );
-  const data = await response.json();
-  console.log(data);
-  return data;
+import { getCityWeather } from "./api.js";
+
+const getCityNameElm = document.querySelector(".cityName");
+const getWeatherImgElm = document.querySelector(".weatherImg");
+console.log(getCityNameElm);
+
+async function init() {
+  const weather = await getCityWeather();
+
+  // render
+
+  console.log(weather);
 }
 
-weatherApi();
+init();
